be0aaee1d6e354207f4c4d15ec54caf83df4001e
Longfish-Meng <loongfish@foxmail.com>
2021-05-05 01:07:18 -0700

    New exercise

---
+++ ch02/sec2_4/ex2_272829.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    // ex27
+    // int i = -1;
+    // int i = -1, &r = 0;
+    // const int i = -1, &r = 0;
+    // const int *const p3 = &i2;
+    // const int &const r2;
+    // const int i2 = i, &r = i;
+
+    // ex28
+    // int i, *const cp;
+    const int *p;
+
+    return 0;
+}
